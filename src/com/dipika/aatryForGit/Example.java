83b947d1f3e4a368513942db590769e4f4fd7dd3
Dipika Upadhaya <ramesh.sigdel@infoworks-tn.com>
2022-12-29 22:09:53 -0600

    New test package

---
+++ src/com/dipika/aatryForGit/Example.java
@@ -0,0 +1,9 @@
+package com.dipika.aatryForGit;
+
+public class Example {
+    public static void main(String[] args) {
+        for(int i=0; i<20; i++){
+            System.out.println(i);
+        }
+    }
+}
