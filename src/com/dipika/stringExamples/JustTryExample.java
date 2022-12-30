fca2230cab7240590f5714782afc396678c960e1
Dipika Upadhaya <ramesh.sigdel@infoworks-tn.com>
2022-12-29 21:42:29 -0600

    JustTryExample file added

---
+++ src/com/dipika/stringExamples/JustTryExample.java
@@ -0,0 +1,8 @@
+package com.dipika.stringExamples;
+
+public class JustTryExample {
+    public static void main(String[] args) {
+        String str= "Deepika";
+        System.out.println("The length of string is: "+str.length());
+    }
+}
